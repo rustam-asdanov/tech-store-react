7ee34d9db991586b674a2a6b6a45a8e381040e2e
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-09-10 21:30:28 +0200

    create product list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProductList from "./product-management/ProductList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World!</p>
       </header>
+
+      <Router>
+        <Link to="/productList">
+          <button>Go To Product List</button>
+        </Link>
+        <Routes>
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/product-management/ProductList.js
@@ -0,0 +1,13 @@
+function ProductList() {
+  return (
+    <div>
+      <ul>
+        <li>product one</li>
+        <button>Edit</button>
+        <button>Delete</button>
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
